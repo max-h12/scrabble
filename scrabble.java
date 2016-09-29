1af3537c63940cbb7493720618aabbee8f763a7a
lpaznboy99 <lpaznboy99@hotmail.com>
2016-09-29 14:37:42 -0400

    Add files via upload

--- scrabble.java
+++ scrabble.java
@@ -1,11 +1,131 @@
-import java.util.*;
-import java.io.*;
-
 public class scrabble{
-ArrayList<String>bag = new ArrayList<String>();
 
-   public static void createBag (ArrayList bag){
-   
-   }   
-   
+
+
+
+public SparseMatrix<Tile> board = new SparseMatrix(15,15);
+Tile blanky = new Tile(' ');
+for(int i = 0; i < 15; i++)
+{
+   for(int j = 0; j <15; j++)
+   {
+      board.add(i, j, blanky);
+   }
+}
+
+
+
+public static void addTile(Tile t)
+{
+   board.set(i, j, t);
+}
+
+ArrayList<Tile> wordBag = new ArrayList<Tile>();
+for(int counter = 0; count < 9; count++)
+{
+   list.add(new Tile("A"));
+}
+for(int counter = 9; count < 11; count++)
+{
+   list.add(new Tile("B"));
+}
+for(int counter = 11; count < 13; count++)
+{
+   list.add(new Tile("C"));
+}
+for(int counter = 13; count < 18; count++)
+{
+   list.add(new Tile("D"));
+}
+for(int counter = 18; count < 31; count++)
+{
+   list.add(new Tile("E"));
+}
+for(int counter = 31; count < 33; count++)
+{
+   list.add(new Tile("F"));
+}
+for(int counter = 33; count < 36; count++)
+{
+   list.add(new Tile("G"));
+}
+for(int counter = 36; count < 40; count++)
+{
+   list.add(new Tile("H"));
+}
+for(int counter = 40; count < 48; count++)
+{
+   list.add(new Tile("I"));
+}
+for(int counter = 48; count < 49; count++)
+{
+   list.add(new Tile("J"));
+}
+for(int counter = 49; count < 50; count++)
+{
+   list.add(new Tile("K"));
+}
+for(int counter = 50; count < 54; count++)
+{
+   list.add(new Tile("L"));
+}
+for(int counter = 54; count < 56; count++)
+{
+   list.add(new Tile("M"));
+}
+for(int counter = 56; count < 61; count++)
+{
+   list.add(new Tile("N"));
+}
+for(int counter = 61; count < 69; count++)
+{
+   list.add(new Tile("O"));
+}
+for(int counter = 69; count < 71; count++)
+{
+   list.add(new Tile("P"));
+}
+for(int counter = 71; count < 72; count++)
+{
+   list.add(new Tile("Q"));
+}
+for(int counter = 72; count < 78; count++)
+{
+   list.add(new Tile("R"));
+}
+for(int counter = 78; count < 83; count++)
+{
+   list.add(new Tile("S"));
+}
+for(int counter = 83; count < 90; count++)
+{
+   list.add(new Tile("T"));
+}
+for(int counter = 90; count < 94; count++)
+{
+   list.add(new Tile("U"));
+}
+for(int counter = 94; count < 96; count++)
+{
+   list.add(new Tile("V"));
+}
+for(int counter = 96; count < 98; count++)
+{
+   list.add(new Tile("W"));
+}
+for(int counter = 98; count < 99; count++)
+{
+   list.add(new Tile("X"));
+}
+for(int counter = 99; count < 101; count++)
+{
+   list.add(new Tile("Y"));
+}
+for(int counter = 101; count < 102; count++)
+{
+   list.add(new Tile("Z"));
+}
+for(int counter = 102; count < 104; count++)
+{
+   list.add(new Tile("BL"));
 }
